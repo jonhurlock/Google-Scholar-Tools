0312d30c421c2ede8832d7bf748b1b4bf4ec3446
jonhurlock <jonhurlock@Jonathans-MacBook-Pro.local>
2014-11-04 11:14:02 +0000

    added some comments to scrape_publication.py

--- scrape_publication.py
+++ scrape_publication.py
@@ -2,6 +2,14 @@ from bs4 import BeautifulSoup # used for parsing and doing some funky HTML stuff
 import urllib # used for grabbing data from URLs
 import unicodedata # for converting unicode to ascii
 
+# The following page scrapes important data from a google publication page.
+# To run => python scrape_publication.py
+# Output =>
+# URL: http://scholar.google.co.uk/citations?view_op=view_citation&hl=en&user=pu0mIWgAAAAJ&citation_for_view=pu0mIWgAAAAJ:u5HHmVD_uO8C
+# Title: Searching Twitter: Separating the Tweet from the Chaff.
+# Authors:  ['Jonathan Hurlock', 'Max L Wilson']
+# Desription: Abstract Within the millions of digital communications posted in online social networks, thereis undoubtedly some valuable and useful information. Although a large portion of socialmedia content is considered to be babble, research shows that people share useful links,provide recommendations to friends, answer questions, and solve problems. In this paper,we report on a qualitative investigation into the different factors that make tweets 'useful'and'not useful'for a set of common search tasks. The investigation found 16 features that help...
+
 # go get a URL
 url_to_scrape = "http://scholar.google.co.uk/citations?view_op=view_citation&hl=en&user=pu0mIWgAAAAJ&citation_for_view=pu0mIWgAAAAJ:u5HHmVD_uO8C"
 f = urllib.urlopen(url_to_scrape)
